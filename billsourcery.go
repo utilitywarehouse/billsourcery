cd41a280fdd6e595ff8c7e9db20db05a02fc27bc
Martin Garton <mgarton@utilitywarehouse.co.uk>
2017-01-12 08:45:43 +0000

    Split string processor out
    Move "string constants" processor into its own file

--- billsourcery.go
+++ billsourcery.go
@@ -143,31 +143,6 @@ func walkSource(sourceRoot string, proc processor) error {
 	})
 }
 
-type stringConsts struct{}
-
-func (lp *stringConsts) end() {}
-
-func (lp *stringConsts) process(path string) error {
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	l := equilex.NewLexer(transform.NewReader(f, charmap.Windows1252.NewDecoder()))
-
-	for {
-		tok, lit := l.Scan()
-
-		switch tok {
-		case equilex.EOF:
-			return nil
-		case equilex.StringConstant:
-			fmt.Println(lit[1 : len(lit)-1])
-		}
-	}
-}
-
 type identFreq struct{}
 
 func (ifr *identFreq) end() {}
