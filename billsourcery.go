798765ad08f4ec89b5a4216089369a9b55d60e13
Martin Garton <mgarton@utilitywarehouse.co.uk>
2017-01-12 08:44:23 +0000

    Split lex checker processor out
    Move lex check processor into its own file

--- billsourcery.go
+++ billsourcery.go
@@ -143,38 +143,6 @@ func walkSource(sourceRoot string, proc processor) error {
 	})
 }
 
-type lexCheck struct {
-	anyErrors bool
-}
-
-func (lp *lexCheck) end() {
-	if !lp.anyErrors {
-		log.Println("no lexer errors.")
-	}
-}
-
-func (lp *lexCheck) process(path string) error {
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	l := equilex.NewLexer(transform.NewReader(f, charmap.Windows1252.NewDecoder()))
-
-	for {
-		tok, lit := l.Scan()
-
-		switch tok {
-		case equilex.EOF:
-			return nil
-		case equilex.Illegal:
-			lp.anyErrors = true
-			log.Printf("illegal token in file '%s' : '%v'\n", path, lit)
-		}
-	}
-}
-
 type stringConsts struct{}
 
 func (lp *stringConsts) end() {}
