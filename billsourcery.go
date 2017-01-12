faf56a8ed0247995aa89293e1d9d23f510480fb0
Martin Garton <mgarton@utilitywarehouse.co.uk>
2017-01-12 09:06:34 +0000

    Split statement type out
    Move statement type into its own file.

--- billsourcery.go
+++ billsourcery.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/jawher/mow.cli"
-	"github.com/utilitywarehouse/equilex"
 )
 
 type processor interface {
@@ -138,25 +136,4 @@ func walkSource(sourceRoot string, proc processor) error {
 		}
 		return nil
 	})
-}
-
-type token struct {
-	tok equilex.Token
-	lit string
-}
-
-type statement struct {
-	tokens []token
-}
-
-func (stmt *statement) String() string {
-	var buf bytes.Buffer
-	for _, t := range stmt.tokens {
-		buf.WriteString(t.lit)
-	}
-	return buf.String()
-}
-
-func (stmt *statement) add(tok equilex.Token, lit string) {
-	stmt.tokens = append(stmt.tokens, token{tok, lit})
 }
---
+++ statement.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+
+	"github.com/utilitywarehouse/equilex"
+)
+
+type token struct {
+	tok equilex.Token
+	lit string
+}
+
+type statement struct {
+	tokens []token
+}
+
+func (stmt *statement) String() string {
+	var buf bytes.Buffer
+	for _, t := range stmt.tokens {
+		buf.WriteString(t.lit)
+	}
+	return buf.String()
+}
+
+func (stmt *statement) add(tok equilex.Token, lit string) {
+	stmt.tokens = append(stmt.tokens, token{tok, lit})
+}
