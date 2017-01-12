ff3f8669f9a571ddb3f1b97e1d12f23cabf4bcc4
Martin Garton <mgarton@utilitywarehouse.co.uk>
2017-01-12 08:53:13 +0000

    Split executions processor
    Most executions processor into its own file

--- billsourcery.go
+++ billsourcery.go
@@ -9,8 +9,6 @@ import (
 
 	"github.com/jawher/mow.cli"
 	"github.com/utilitywarehouse/equilex"
-	"golang.org/x/text/encoding/charmap"
-	"golang.org/x/text/transform"
 )
 
 type processor interface {
@@ -140,46 +138,6 @@ func walkSource(sourceRoot string, proc processor) error {
 		}
 		return nil
 	})
-}
-
-type executions struct{}
-
-func (ex *executions) end() {}
-
-func (ex *executions) process(path string) error {
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	l := equilex.NewLexer(transform.NewReader(f, charmap.Windows1252.NewDecoder()))
-
-	var stmt *statement
-
-	for {
-		tok, lit := l.Scan()
-
-		switch tok {
-		case equilex.EOF:
-			if stmt != nil {
-				log.Println(stmt.String())
-			}
-			return nil
-		case equilex.Execute:
-			stmt = &statement{}
-			stmt.add(tok, lit)
-		case equilex.NewLine:
-			if stmt != nil {
-				log.Println(stmt.String())
-			}
-			stmt = nil
-		default:
-			if stmt != nil {
-				stmt.add(tok, lit)
-			}
-		}
-	}
 }
 
 type token struct {
