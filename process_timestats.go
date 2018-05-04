3ebbc79481340badd3d33b83ab458c0074e37bf3
Martin Garton <mgarton@utilitywarehouse.co.uk>
2018-05-04 11:00:01 +0100

    Split out timestats
    Split the image parts out of timestats in preparation for other uses of
    the data that are not images.

---
+++ process_timestats.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"log"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+type timeStatsEntry struct {
+	Time    time.Time       `json:"timestamp"`
+	Results *statsProcessor `json:"results"`
+}
+
+type cacheEntry struct {
+	TimeStats *timeStatsEntry `json:"stats,omitempty"`
+	Err       string          `json:"error,omitempty"`
+}
+
+type timeStatsProcessor struct {
+	AllStats map[string][]*timeStatsEntry
+
+	cacheDB          string
+	earliestRevision string
+	branches         []string
+	outfile          string
+}
+
+func (lp *timeStatsProcessor) processAll(sourceRoot string) error {
+	for _, b := range lp.branches {
+		if err := lp.processBranch(b, sourceRoot); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (lp *timeStatsProcessor) processBranch(branch string, sourceRoot string) error {
+	cmd := exec.Command("git", "checkout", branch)
+	cmd.Dir = sourceRoot
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	cmd = exec.Command("git", "log", `--format=%H %cI`)
+	cmd.Dir = sourceRoot
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+
+	cs := &timestatsCache{lp.cacheDB}
+
+	s := bufio.NewScanner(bytes.NewReader(out))
+	for s.Scan() {
+		spl := strings.Split(s.Text(), " ")
+		revision := spl[0]
+		date, err := time.Parse(time.RFC3339, spl[1])
+		if err != nil {
+			panic(err)
+		}
+
+		c, err := cs.get(revision)
+		if err != nil {
+			return err
+		}
+		if c != nil {
+			log.Printf("already have stats for revision %s\n", revision)
+			if c.Err != "" {
+				log.Printf("cached stats for revision %s is an error\n", revision)
+			} else {
+				lp.AllStats[branch] = append(lp.AllStats[branch], c.TimeStats)
+			}
+		} else {
+
+			log.Printf("creating stats for revision %s\n", revision)
+
+			cmd = exec.Command("git", "checkout", revision)
+			cmd.Dir = sourceRoot
+			err = cmd.Run()
+			if err != nil {
+				return err
+			}
+
+			sp := &statsProcessor{}
+			err = sp.processAll(sourceRoot)
+			var tse *timeStatsEntry
+			if err != nil {
+				log.Printf("revision %s was in error (will still cache): %s\n", revision, err.Error())
+				cs.put(revision, &cacheEntry{Err: err.Error()})
+			} else {
+				tse = &timeStatsEntry{date, sp}
+				lp.AllStats[branch] = append(lp.AllStats[branch], tse)
+				cs.put(revision, &cacheEntry{TimeStats: tse})
+			}
+		}
+		if revision == lp.earliestRevision {
+			log.Printf("Exiting because earliest revision '%s' reached\n", revision)
+			break
+		}
+	}
+
+	cmd = exec.Command("git", "checkout", "master")
+	cmd.Dir = sourceRoot
+	err = cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type timestatsCache struct {
+	filename string
+}
+
+func (tsc *timestatsCache) put(revision string, c *cacheEntry) error {
+	db, err := bolt.Open(tsc.filename, 0600, nil)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	if err := db.Update(func(tx *bolt.Tx) error {
+		value, err := json.Marshal(c)
+		if err != nil {
+			return err
+		}
+
+		b, err := tx.CreateBucketIfNotExists([]byte("timestats"))
+		if err != nil {
+			return err
+		}
+
+		return b.Put([]byte(revision), value)
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tsc timestatsCache) get(rev string) (*cacheEntry, error) {
+	db, err := bolt.Open(tsc.filename, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	var s *cacheEntry
+	if err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("timestats"))
+		if b != nil {
+			data := b.Get([]byte(rev))
+			if data != nil {
+				s = &cacheEntry{}
+				err := json.Unmarshal(data, s)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return s, nil
+}
--- process_timestats_image.go
+++ process_timestats_image.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"image/color"
 	"log"
-	"os/exec"
-	"strings"
-	"time"
 
-	"github.com/boltdb/bolt"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
@@ -19,166 +12,23 @@ import (
 )
 
 func newTimeStatsImageProcessor(cacheDB string, earliest string, branches []string, outfile string) *timeStatsImageProcessor {
-	return &timeStatsImageProcessor{AllStats: make(map[string][]*timeStatsEntry), cacheDB: cacheDB, earliestRevision: earliest, branches: branches, outfile: outfile}
-}
-
-type timeStatsEntry struct {
-	Time    time.Time       `json:"timestamp"`
-	Results *statsProcessor `json:"results"`
-}
-
-type cacheEntry struct {
-	TimeStats *timeStatsEntry `json:"stats,omitempty"`
-	Err       string          `json:"error,omitempty"`
+	return &timeStatsImageProcessor{timeStatsProcessor{AllStats: make(map[string][]*timeStatsEntry), cacheDB: cacheDB, earliestRevision: earliest, branches: branches, outfile: outfile}}
 }
 
 type timeStatsImageProcessor struct {
-	AllStats map[string][]*timeStatsEntry
-
-	cacheDB          string
-	earliestRevision string
-	branches         []string
-	outfile          string
+	timeStatsProcessor
 }
 
-func (lp *timeStatsImageProcessor) processAll(sourceRoot string) error {
-	for _, b := range lp.branches {
-		if err := lp.processBranch(b, sourceRoot); err != nil {
-			return err
-		}
+type IntTicker struct{}
+
+func (it *IntTicker) Ticks(min float64, max float64) []plot.Tick {
+	def := plot.DefaultTicks{}
+	ticks := def.Ticks(min, max)
+	for i, t := range ticks {
+		t.Label = fmt.Sprintf("%d", uint64(t.Value))
+		ticks[i] = t
 	}
-	return nil
-}
-
-func (lp *timeStatsImageProcessor) processBranch(branch string, sourceRoot string) error {
-	cmd := exec.Command("git", "checkout", branch)
-	cmd.Dir = sourceRoot
-	err := cmd.Run()
-	if err != nil {
-		return err
-	}
-
-	cmd = exec.Command("git", "log", `--format=%H %cI`)
-	cmd.Dir = sourceRoot
-	out, err := cmd.Output()
-	if err != nil {
-		return err
-	}
-
-	cs := &timestatsCache{lp.cacheDB}
-
-	s := bufio.NewScanner(bytes.NewReader(out))
-	for s.Scan() {
-		spl := strings.Split(s.Text(), " ")
-		revision := spl[0]
-		date, err := time.Parse(time.RFC3339, spl[1])
-		if err != nil {
-			panic(err)
-		}
-
-		c, err := cs.get(revision)
-		if err != nil {
-			return err
-		}
-		if c != nil {
-			log.Printf("already have stats for revision %s\n", revision)
-			if c.Err != "" {
-				log.Printf("cached stats for revision %s is an error\n", revision)
-			} else {
-				lp.AllStats[branch] = append(lp.AllStats[branch], c.TimeStats)
-			}
-		} else {
-
-			log.Printf("creating stats for revision %s\n", revision)
-
-			cmd = exec.Command("git", "checkout", revision)
-			cmd.Dir = sourceRoot
-			err = cmd.Run()
-			if err != nil {
-				return err
-			}
-
-			sp := &statsProcessor{}
-			err = sp.processAll(sourceRoot)
-			var tse *timeStatsEntry
-			if err != nil {
-				log.Printf("revision %s was in error (will still cache): %s\n", revision, err.Error())
-				cs.put(revision, &cacheEntry{Err: err.Error()})
-			} else {
-				tse = &timeStatsEntry{date, sp}
-				lp.AllStats[branch] = append(lp.AllStats[branch], tse)
-				cs.put(revision, &cacheEntry{TimeStats: tse})
-			}
-		}
-		if revision == lp.earliestRevision {
-			log.Printf("Exiting because earliest revision '%s' reached\n", revision)
-			break
-		}
-	}
-
-	cmd = exec.Command("git", "checkout", "master")
-	cmd.Dir = sourceRoot
-	err = cmd.Run()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-type timestatsCache struct {
-	filename string
-}
-
-func (tsc *timestatsCache) put(revision string, c *cacheEntry) error {
-	db, err := bolt.Open(tsc.filename, 0600, nil)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	if err := db.Update(func(tx *bolt.Tx) error {
-		value, err := json.Marshal(c)
-		if err != nil {
-			return err
-		}
-
-		b, err := tx.CreateBucketIfNotExists([]byte("timestats"))
-		if err != nil {
-			return err
-		}
-
-		return b.Put([]byte(revision), value)
-	}); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (tsc timestatsCache) get(rev string) (*cacheEntry, error) {
-	db, err := bolt.Open(tsc.filename, 0600, nil)
-	if err != nil {
-		return nil, err
-	}
-	defer db.Close()
-
-	var s *cacheEntry
-	if err := db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("timestats"))
-		if b != nil {
-			data := b.Get([]byte(rev))
-			if data != nil {
-				s = &cacheEntry{}
-				err := json.Unmarshal(data, s)
-				if err != nil {
-					return err
-				}
-			}
-		}
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-	return s, nil
+	return ticks
 }
 
 func (lp *timeStatsImageProcessor) end() error {
@@ -271,16 +121,4 @@ func (lp *timeStatsImageProcessor) end() error {
 	}
 	log.Printf("saved output to '%s'\n", lp.outfile)
 	return nil
-}
-
-type IntTicker struct{}
-
-func (it *IntTicker) Ticks(min float64, max float64) []plot.Tick {
-	def := plot.DefaultTicks{}
-	ticks := def.Ticks(min, max)
-	for i, t := range ticks {
-		t.Label = fmt.Sprintf("%d", uint64(t.Value))
-		ticks[i] = t
-	}
-	return ticks
 }
