30edb605f8dd026d8b35ca1b1b7bb89f910b9076
Martin Garton <garton@gmail.com>
2024-10-15 19:39:37 +0100

    Add output for unused fields to script

--- unused.sh
+++ unused.sh
@@ -2,3 +2,4 @@
 echo "MATCH (n:Table) where not exists { match (n:Table)<-[r:references]-(m) where NOT m:Index and NOT m:Field AND r IS NOT NULL} RETURN distinct n.name as table_name" | cypher-shell  > unused_tables.csv
 echo "MATCH (i:Index)-[:references]->(t:Table) WHERE NOT (i)<-[:references]-() RETURN t.name as table_name, i.name as index_name order by table_name, index_name" | cypher-shell > unused_indexes.csv
 echo "MATCH (pp:PublicProcedure) WHERE NOT (pp)<-[:references]-() and not pp:Used RETURN pp.name as public_procedure_name order by public_procedure_name" | cypher-shell > unused_pp.csv
+echo "MATCH (f:Field)-[:references]->(t:Table) WHERE NOT (f)<-[:references]-() RETURN t.name as table_name, f.name as field_name order by t.name, f.name" | cypher-shell > unused_fields.csv
